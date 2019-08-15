c744ed0cc7c69938cca31efca850bdbd4e9f2450
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-15 11:31:49 -0400

    Make board rendering function private

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const gameBoard = ((cellNumber) => {
-    for (let i = 0; i < cellNumber; i++) {
-        let gridContainer = document.querySelector('.grid-container');
-        let gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridContainer.appendChild(gridCell);
-    }
-})(9);
+const gameBoard = (() => {
+    const renderBoard = ((cellNumber) => {
+        for (let i = 0; i < cellNumber; i++) {
+            let gridContainer = document.querySelector('.grid-container');
+            let gridCell = document.createElement('div');
+            gridCell.classList.add('grid-cell');
+            gridContainer.appendChild(gridCell);
+        }
+    })(9);
+    
+})();
 
 // const playerEx
-    
+
 // const playerOh
 
 // toggle between players on each click
