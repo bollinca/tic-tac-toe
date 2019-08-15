a08c2a5982b250fedf6ab5b746db7e91726e44e1
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-15 11:20:44 -0400

    Create 3x3 gameboard

---
+++ script.js
@@ -0,0 +1,14 @@
+const gameBoard = ((cellNumber) => {
+    for (let i = 0; i < cellNumber; i++) {
+        let gridContainer = document.querySelector('.grid-container');
+        let gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridContainer.appendChild(gridCell);
+    }
+})(9);
+
+// const playerEx
+    
+// const playerOh
+
+// toggle between players on each click
