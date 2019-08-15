8ad74308889a15ec156899e35a3caec746ba5e07
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-15 13:26:37 -0400

    Add first draft of player factory

--- script.js
+++ script.js
@@ -3,26 +3,45 @@
 const gameBoard = (() => {
     const cellList = [];
 
-    const test = 'this is a test';
-
     const renderBoard = ((cellNumber) => {
         for (let i = 0; i < cellNumber; i++) {
             let gridContainer = document.querySelector('.grid-container');
             let gridCell = document.createElement('div');
             gridCell.classList.add('grid-cell');
             gridCell.setAttribute('id', `grid-cell-${i + 1}`)
-            cellList.push(`grid-cell-${i + 1}`);
             gridContainer.appendChild(gridCell);
-            //add 'click' listeners
+
+            cellList.push(`grid-cell-${i + 1}`);
         }
     })(9);
 
-    return {cellList};
-
-// const players
-
+    return { cellList };
 })();
 
+const protoPlayer = (team, playerClass) => {
+
+    const cellsClaimed = [];
+    const gridCells = document.querySelectorAll('.grid-cell');
+    const status = 'off';
+
+    const displayController = () => {
+        gridCells.forEach(cell => cell.addEventListener('click', (e) => cellsClaimed.push(e.target.id)));
+        gridCells.forEach(cell => cell.addEventListener('click', (e) => e.target.classList.add(playerClass)));
+    };
+
+    const playerToggle = () => {
+        if (status === 'off') {
+            status = 'on'
+        } else if (status === 'on') {
+            status = 'off';
+        }
+        return {status};
+    };
+
+    return { team, cellsClaimed };
+}
+
+// const players
 // const playerEx
 
 // const playerOh
