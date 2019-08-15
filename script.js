338899effed02d898b2f82f849b76035dd14ea8b
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-15 12:08:44 -0400

    Add identifiers to each cell

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+'use strict'
+
 const gameBoard = (() => {
+    const cellList = [];
+
+    const test = 'this is a test';
+
     const renderBoard = ((cellNumber) => {
         for (let i = 0; i < cellNumber; i++) {
             let gridContainer = document.querySelector('.grid-container');
             let gridCell = document.createElement('div');
             gridCell.classList.add('grid-cell');
+            gridCell.setAttribute('id', `grid-cell-${i + 1}`)
+            cellList.push(`grid-cell-${i + 1}`);
             gridContainer.appendChild(gridCell);
+            //add 'click' listeners
         }
     })(9);
-    
+
+    return {cellList};
+
+// const players
+
 })();
 
 // const playerEx
