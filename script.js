9c6ca40edabdcba64b2029eaf46c917821d757d9
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-16 12:23:07 -0400

    Factor out victory checks, fix player toggle click bug

--- script.js
+++ script.js
@@ -43,35 +43,55 @@ const protoPlayer = (team, playerClass, status = 'off') => {
         }));
     })();
 
-    gridCells.forEach(cell => cell.addEventListener('click', () => {
-        playerToggle();
+    gridCells.forEach(cell => cell.addEventListener('mousedown', (e) => {
+        if (!e.target.classList.contains('claimed')) {
+            playerToggle();
+        }
     }
     ));
 
-    const isWinner = (() => {
+    function checkVerticalVictory(xValue, yValue) {
+        if (cellsClaimed.toString().includes(`x:${xValue} y:${yValue}`)
+            && cellsClaimed.toString().includes(`x:${xValue} y:${yValue + 1}`)
+            && cellsClaimed.toString().includes(`x:${xValue} y:${yValue + 2}`)) {
+            return true;
+        }
+    }
+
+    function checkHorizontalVictory(xValue, yValue) {
+        if (cellsClaimed.toString().includes(`x:${xValue} y:${yValue}`)
+            && cellsClaimed.toString().includes(`x:${xValue + 1} y:${yValue}`)
+            && cellsClaimed.toString().includes(`x:${xValue + 2} y:${yValue}`)) {
+            return true;
+        }
+    }
+
+    function checkDiagonalVictory() {
+        if (cellsClaimed.toString().includes(`x:${1} y:${1}`)
+            && cellsClaimed.toString().includes(`x:${2} y:${2}`)
+            && cellsClaimed.toString().includes(`x:${3} y:${3}`)) {
+            return true;
+        } else if (cellsClaimed.toString().includes(`x:${1} y:${3}`)
+            && cellsClaimed.toString().includes(`x:${2} y:${2}`)
+            && cellsClaimed.toString().includes(`x:${3} y:${1}`)) {
+            return true;
+        }
+    }
+
+    const declareWinner = (() => {
         gridCells.forEach(cell => cell.addEventListener('click', (e) => {
             if (cellsClaimed.length >= 3) {
                 for (let i = 1; i <= 3; i++) {
                     for (let j = 1; j <= 3; j++) {
-                        if (cellsClaimed.toString().includes(`x:${i} y:${j}`)
-                            && cellsClaimed.toString().includes(`x:${i} y:${j + 1}`)
-                            && cellsClaimed.toString().includes(`x:${i} y:${j + 2}`)) {
-                            console.log(`${team} victory!`);
-                        } else if (cellsClaimed.toString().includes(`x:${i} y:${j}`)
-                            && cellsClaimed.toString().includes(`x:${i + 1} y:${j}`)
-                            && cellsClaimed.toString().includes(`x:${i + 2} y:${j}`)) {
-                            console.log(`${team} victory!`);
+                        if (checkVerticalVictory(i, j)) {
+                            console.log(`${team} victory`)
+                        } else if (checkHorizontalVictory(i, j)) {
+                            console.log(`${team} victory`)
                         }
                     }
                 }
-                if (cellsClaimed.toString().includes(`x:${1} y:${1}`)
-                    && cellsClaimed.toString().includes(`x:${2} y:${2}`)
-                    && cellsClaimed.toString().includes(`x:${3} y:${3}`)) {
-                    console.log(`${team} victory!`);
-                } else if (cellsClaimed.toString().includes(`x:${1} y:${3}`)
-                    && cellsClaimed.toString().includes(`x:${2} y:${2}`)
-                    && cellsClaimed.toString().includes(`x:${3} y:${1}`)) {
-                    console.log(`${team} victory!`);
+                if (checkDiagonalVictory()) {
+                    console.log(`${team} victory`);
                 }
             }
         }));
