3fbd6472ea7f12c7783cb10cca5a2b68efec2071
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-17 13:20:42 -0400

    Delete old script code. Begin rewrite to clean up logic

--- script.js
+++ script.js
@@ -1,130 +1,52 @@
 'use strict'
 
+const protoPlayer = (name, team) => {
+    return { name, team };
+};
+
 const gameBoard = (() => {
-    const renderBoard = ((cellNumber) => {
-        for (let i = 0; i < cellNumber; i++) {
-            let gridContainer = document.querySelector('.grid-container');
-            let gridCell = document.createElement('div');
+    const gridCellArray = [];
+
+    const createBoard = ((totalCells) => {
+        for (let i = 1; i <= totalCells; i++) {
+
+            const gridCell = document.createElement('div');
             gridCell.classList.add('grid-cell');
-            if (i < 3) {
-                gridCell.setAttribute('id', `x:${i + 1} y:3`)
-            } else if (i < 6) {
-                gridCell.setAttribute('id', `x:${i + 1 - 3} y:2`)
-            } else if (i < 9) {
-                gridCell.setAttribute('id', `x:${i + 1 - 6} y:1`)
-            }
+            gridCell.setAttribute('id', `${i}`);
 
+            const gridContainer = document.querySelector('.grid-container');
             gridContainer.appendChild(gridCell);
+
+            gridCellArray.push(gridCell.attributes.id);
         }
-    })(9);
+    })(9)
+
+    return { gridCellArray };
+})()
+
+const gameState = (() => {
+    let matchComplete = false;
+
+    const isBoardFull = (() => {
+        //return (9 cells claimed)
+    })()
+
+    return { matchComplete, isBoardFull };
 })();
 
+/*
+refactoring notes:
 
-const protoPlayer = (team, playerClass, status) => {
+player should have:
+ - name
+ - team-assignment
 
-    const cellsClaimed = [];
-    const gridCells = document.querySelectorAll('.grid-cell');
+gameBoard should have:
+ - cell array
+ - display changes
 
-    const playerToggle = () => {
-        if (status === 'off') {
-            return status = 'on';
-        } else if (status === 'on') {
-            return status = 'off';
-        }
-    };
+gameState should have:
+ - player turn (previously 'status')
+ - victory checks
 
-    const displayController = (() => {
-        gridCells.forEach(cell => cell.addEventListener('click', (e) => {
-            if (status === 'on' && !(e.target.classList.contains('claimed'))) {
-                e.target.classList.add(playerClass)
-                e.target.classList.add('claimed');
-                cellsClaimed.push(e.target.id);
-            }
-        }));
-    })();
-
-    gridCells.forEach(cell => cell.addEventListener('mousedown', (e) => {
-        if (!e.target.classList.contains('claimed')) {
-            playerToggle();
-        }
-    }
-    ));
-
-    function checkVerticalVictory(xValue, yValue) {
-        if (cellsClaimed.toString().includes(`x:${xValue} y:${yValue}`)
-            && cellsClaimed.toString().includes(`x:${xValue} y:${yValue + 1}`)
-            && cellsClaimed.toString().includes(`x:${xValue} y:${yValue + 2}`)) {
-            return true;
-        }
-    }
-
-    function checkHorizontalVictory(xValue, yValue) {
-        if (cellsClaimed.toString().includes(`x:${xValue} y:${yValue}`)
-            && cellsClaimed.toString().includes(`x:${xValue + 1} y:${yValue}`)
-            && cellsClaimed.toString().includes(`x:${xValue + 2} y:${yValue}`)) {
-            return true;
-        }
-    }
-
-    function checkDiagonalVictory() {
-        if (cellsClaimed.toString().includes(`x:${1} y:${1}`)
-            && cellsClaimed.toString().includes(`x:${2} y:${2}`)
-            && cellsClaimed.toString().includes(`x:${3} y:${3}`)) {
-            return true;
-        } else if (cellsClaimed.toString().includes(`x:${1} y:${3}`)
-            && cellsClaimed.toString().includes(`x:${2} y:${2}`)
-            && cellsClaimed.toString().includes(`x:${3} y:${1}`)) {
-            return true;
-        }
-    }
-
-    function clearGridListeners() {
-        const gridContainer = document.querySelector('.grid-container');
-        const gridClone = gridContainer.cloneNode(true);
-        gridContainer.parentNode.replaceChild(gridClone, gridContainer);
-    }
-
-    const declareWinner = (() => {
-        gridCells.forEach(cell => cell.addEventListener('click', (e) => {
-            const headline = document.querySelector('h1');
-            const MAX_MOVES = 5;
-            if (cellsClaimed.length >= 3) {
-                for (let i = 1; i <= 3; i++) {
-                    for (let j = 1; j <= 3; j++) {
-                        if (checkVerticalVictory(i, j)) {
-                            headline.textContent = `Winner: Team ${team}!`
-                            clearGridListeners();
-                        } else if (checkHorizontalVictory(i, j)) {
-                            headline.textContent = `Winner: Team ${team}!`
-                            clearGridListeners();
-                        } else if (cellsClaimed.length === MAX_MOVES) {
-                            headline.textContent = 'It\'s a Tie!';
-                            clearGridListeners();
-                        }
-                    }
-                }
-                if (checkDiagonalVictory()) {
-                    headline.textContent = `Winner: Team ${team}!`
-                    clearGridListeners();
-                } else if (cellsClaimed.length === MAX_MOVES) {
-                    headline.textContent = 'It\'s a Tie!';
-                    clearGridListeners();
-                }
-            }
-        }));
-    })();
-    return { team, cellsClaimed, };
-}
-
-const initialPlayers = (() => {
-    const playerX = protoPlayer('X', 'active-x', 'on');
-    const playerO = protoPlayer('O', 'active-o', 'off');
-
-    return { playerX, playerO }
-})();
-
-const refreshAssignment = (() => {
-    const refreshPage = () => window.location.reload();
-    const clearButton = document.querySelector('#clear')
-    clearButton.addEventListener('click', refreshPage);
-})();
+*/
